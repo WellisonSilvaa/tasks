616645f71662f4b887fb70e5ee0175c65ee287e3
WellisonSilvaa <wonsilva100@gmail.com>
2023-06-15 22:40:04 -0300

    criado o FlatList

--- src/commonStyles.js
+++ src/commonStyles.js
@@ -2,6 +2,8 @@ import Lato from '../assets/fonts/Lato/Lato-BoldItalic.ttf'
 export default {
     fontFamily: Lato,
     colors: {
-        secondary: '#FFF'
+        secondary: '#FFF',
+        mainText: '#222',
+        subText: '#555',
     }
 }
